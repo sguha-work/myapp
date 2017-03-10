d5e66d34a914c810761c2b85f976f5319d61d645
sguha-work <sguha1988.life@gmail.com>
2017-03-10 17:31:10 +0530

    page design continues

--- www/js/routes.js
+++ www/js/routes.js
@@ -58,6 +58,12 @@ angular.module('app.routes', [])
     controller: 'friendsCtrl'
   })
 
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'homeCtrl'
+  })
+
 $urlRouterProvider.otherwise('/login')
 
   
